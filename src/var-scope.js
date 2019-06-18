478f069b9986ba4e93c29a312fa6ebc875c1f711
Yoyo San <veselu@gmail.com>
2019-06-18 14:28:54 +0300

    Add more examples for variables scope.

--- src/var-scope.js
+++ src/var-scope.js
@@ -1,18 +1,18 @@
 function test(bool) {
     if (bool) {
-        let foo = 'test'; // hoisting means the variable declaration
+        var foo = 'test'; // hoisting means the variable declaration
                         // is put at the start of the function
 
+        // foo = 'test'; // foo will become a global variable
         // let/const foo = 'test'; // foo will only be visible in the if block
             // and not in the else or function block
 
         console.log(foo);
     } else {
-        console.log(foo);
+        console.log(foo); // foo isn't accessible if let or const are used
     }
-
-    console.log(foo);
 }
 
 test(true);
 test(false);
+console.log(foo); // foo is available when it isn't declared using a scope
