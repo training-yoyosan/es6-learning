d0268bd5695729209b19b9d7ddb9db2090bb70e8
Yoyo San <veselu@gmail.com>
2019-06-19 12:52:16 +0300

    Update var scope with guidelines.

--- src/var-scope.js
+++ src/var-scope.js
@@ -16,3 +16,11 @@ function test(bool) {
 test(true);
 test(false);
 console.log(foo); // foo is available when it isn't declared using a scope
+
+/**
+ * Jeffrey uses:
+ *
+ * - let, by default, for all variables
+ * - const only when reassignment is not needed
+ * - var not at all. Maybe only at the top level, for some variables that need to be global.
+ */
